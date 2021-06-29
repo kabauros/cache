8017bc977dc1523ac00431aa2cb0ba7d2da46280
Marcos <mcastro@uolinc.com>
2021-06-29 10:46:43 -0300

    retornando a anotação de teste

--- src/test/kotlin/com/cache/cache/CacheApplicationTests.kt
+++ src/test/kotlin/com/cache/cache/CacheApplicationTests.kt
@@ -1,11 +1,12 @@
 package com.cache.cache
 
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class CacheApplicationTests {
 
-
+	@Test
 	fun contextLoads() {
 	}
 
